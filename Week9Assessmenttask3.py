0f40bc196fb98f4f1c67f68ed2e2b212d55d06e6
knyamusevya-hash <knyamusevya@icstars.org>
2025-11-07 14:08:14 -0600

    Add files via upload

---
+++ Week9Assessmenttask3.py
@@ -0,0 +1,23 @@
+#Use the template below to get started
+
+url = endpoint
+response = requests.get(l)
+
+if response.status_code == “ok”:
+data = response.json()
+else:
+print(f"Error code: {“error code”}")
+name = data[index of name]
+print(name)
+
+
+return import
+url = endpoint:
+    url= (f"https://api.nationalize.io/?name=nathaniel")
+    response = requests.get(1)
+    if response.status_code == “ok”:
+data = response.json()
+else:
+print(f"Error code: {“error code”}")
+name = data[index of name]
+print(name)
